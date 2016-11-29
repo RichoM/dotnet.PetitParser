68510b381b5c48c5e01cfc6706be7af2f1fb95c2
RichoM <richi.moran@gmail.com>
2016-11-29 08:55:27 -0300

    Added tests for the ArithmeticParser.

--- src/PetitParser.Test/ArithmeticParserTest.cs
+++ src/PetitParser.Test/ArithmeticParserTest.cs
@@ -30,5 +30,114 @@ namespace PetitParser.Test
             Assert.AreEqual(-9.0, pp.Parse("-9"));
             Assert.AreEqual(-9.9, pp.Parse("-9.9"));
         }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            Assert.AreEqual(3.0, pp.Parse("1 + 2"));
+            Assert.AreEqual(3.0, pp.Parse("2 + 1"));
+            Assert.AreEqual(3.3, pp.Parse("1 + 2.3"));
+            Assert.AreEqual(3.3, pp.Parse("2.3 + 1"));
+            Assert.AreEqual(-1.0, pp.Parse("1 + -2"));
+            Assert.AreEqual(-1.0, pp.Parse("-2 + 1"));
+        }
+
+        [TestMethod]
+        public void TestAddMany()
+        {
+            Assert.AreEqual(3.0, pp.Parse("1 + 2"));
+            Assert.AreEqual(6.0, pp.Parse("1 + 2 + 3"));
+            Assert.AreEqual(10.0, pp.Parse("1 + 2 + 3 + 4"));
+            Assert.AreEqual(15.0, pp.Parse("1 + 2 + 3 + 4 + 5"));
+        }
+
+        [TestMethod]
+        public void TestDiv()
+        {
+            Assert.AreEqual(4.0, pp.Parse("12 / 3"));
+            Assert.AreEqual(4.0, pp.Parse("-16 / -4"));
+        }
+
+        [TestMethod]
+        public void TestDivMany()
+        {
+            Assert.AreEqual(50.0, pp.Parse("100 / 2"));
+            Assert.AreEqual(25.0, pp.Parse("100 / 2 / 2"));
+            Assert.AreEqual(5.0, pp.Parse("100 / 2 / 2 / 5"));
+            Assert.AreEqual(1.0, pp.Parse("100 / 2 / 2 / 5 / 5"));
+        }
+
+        [TestMethod]
+        public void TestMul()
+        {
+            Assert.AreEqual(6.0, pp.Parse("2 * 3"));
+            Assert.AreEqual(-8.0, pp.Parse("2 * -4"));
+        }
+
+        [TestMethod]
+        public void TestMulMany()
+        {
+            Assert.AreEqual(2.0, pp.Parse("1 * 2"));
+            Assert.AreEqual(6.0, pp.Parse("1 * 2 * 3"));
+            Assert.AreEqual(24.0, pp.Parse("1 * 2 * 3 * 4"));
+            Assert.AreEqual(120.0, pp.Parse("1 * 2 * 3 * 4 * 5"));
+        }
+
+        [TestMethod]
+        public void TestPow()
+        {
+            Assert.AreEqual(8.0, pp.Parse("2 ^ 3"));
+            Assert.AreEqual(-8.0, pp.Parse("-2 ^ 3"));
+            Assert.AreEqual(-0.125, pp.Parse("-2 ^ -3"));
+        }
+
+        [TestMethod]
+        public void TestPowMany()
+        {
+            Assert.AreEqual(64.0, pp.Parse("4 ^ 3"));
+            Assert.AreEqual(262144.0, pp.Parse("4 ^ 3 ^ 2"));
+            Assert.AreEqual(262144.0, pp.Parse("4 ^ 3 ^ 2 ^ 1"));
+            Assert.AreEqual(262144.0, pp.Parse("4 ^ 3 ^ 2 ^ 1 ^ 0"));
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            Assert.AreEqual(-1.0, pp.Parse("1 - 2"));
+            Assert.AreEqual(0.0, pp.Parse("1.2 - 1.2"));
+            Assert.AreEqual(3.0, pp.Parse("1 - -2"));
+            Assert.AreEqual(1.0, pp.Parse("-1 - -2"));
+        }
+
+        [TestMethod]
+        public void TestSubMany()
+        {
+            Assert.AreEqual(-1.0, pp.Parse("1 - 2"));
+            Assert.AreEqual(-4.0, pp.Parse("1 - 2 - 3"));
+            Assert.AreEqual(-8.0, pp.Parse("1 - 2 - 3 - 4"));
+            Assert.AreEqual(-13.0, pp.Parse("1 - 2 - 3 - 4 - 5"));
+        }
+
+        [TestMethod]
+        public void TestPriority()
+        {
+            Assert.AreEqual(10.0, pp.Parse("2 * 3 + 4"));
+            Assert.AreEqual(14.0, pp.Parse("2 + 3 * 4"));
+            Assert.AreEqual(6.0, pp.Parse("6 / 3 + 4"));
+            Assert.AreEqual(5.0, pp.Parse("2 + 6 / 2"));
+        }
+
+        [TestMethod]
+        public void TestBrackets()
+        {
+            Assert.AreEqual(1.0, pp.Parse("(1)"));
+            Assert.AreEqual(3.0, pp.Parse("(1 + 2)"));
+            Assert.AreEqual(1.0, pp.Parse("((1))"));
+            Assert.AreEqual(3.0, pp.Parse("((1 + 2))"));
+            Assert.AreEqual(14.0, pp.Parse("2 * (3 + 4)"));
+            Assert.AreEqual(20.0, pp.Parse("(2 + 3) * 4"));
+            Assert.AreEqual(1.0, pp.Parse("6 / (2 + 4)"));
+            Assert.AreEqual(4.0, pp.Parse("(2 + 6) / 2"));
+        }
     }
 }
